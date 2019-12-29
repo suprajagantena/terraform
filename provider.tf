858348bb09efd92840864309a89de67c308f8e97
suprajagantena <suprajagantena@gmail.com>
2019-12-29 17:09:50 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   version = "~> 2.0"
   access key = "AKIATOUVDBKIM2ZSZ2QS"
   secret key = "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"
-  region  = "us-east-1"
+  region  = "${var.aws_region}"
 }
