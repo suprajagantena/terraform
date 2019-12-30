e8e1d367d695906a3a7b9312f363047c89f5eb99
suprajagantena <suprajagantena@gmail.com>
2019-12-29 23:17:50 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,15 +5,15 @@ variable "aws_region" {
 
 variable "vpc_cidr" {
   description = "CIDR for the VPC"
-  default = "10.0.0.0/16"
+  default = "10.0.0.0/16,10.0.0.0/18,10.0.0.0/24"
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR for the public subnet"
-  default = "10.0.1.0/24"
+  default = ""
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR for the private subnet"
-  default = "10.0.2.0/24"
+  default = ""
 }
