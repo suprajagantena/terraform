afa8f034a0cd41aeebae5f6a27c8c4230f8a808d
suprajagantena <suprajagantena@gmail.com>
2020-03-13 17:13:25 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   version = "~> 2.0"
-  access_key = "${var.access_key}"
- secret_key = "${var.secret_key}"
-  region  = "${var.aws_region}"
+  access_key = "var.access_key"
+ secret_key = "var.secret_key"
+  region  = "var.aws_region"
 }
